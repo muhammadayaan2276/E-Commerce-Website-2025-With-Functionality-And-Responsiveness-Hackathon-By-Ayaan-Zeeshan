1453de2e8da106b9e942c740acc76bed1a00cac4
ahmednoorani <ahmednoorani258@gmail.com>
2025-02-03 09:36:33 -0800

    again previous bug fixed

--- src/components/ShortSec.tsx
+++ src/components/ShortSec.tsx
@@ -20,7 +20,7 @@ export default  function ShortSec(data:Sectiondata) {
       <div className="space-y-8 w-full flex flex-col justify-center items-center py-8">
         <h1 className="text-4xl font-semibold">{data.title}</h1>
         {data.description && <p className="text-sm text-gray-600">{data.description}</p>}
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 ">
+        <div className="w-full grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 ">
           {data.cardData.map((cardData: CardData, index:number) => (
           
             <Card key={index} {...cardData} />
