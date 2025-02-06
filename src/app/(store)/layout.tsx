156aa68ba885f0a933580a9e2d74d8a023278bb1
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2025-02-07 00:27:21 +0500

    first commit

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,11 +3,11 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { CombinedProvider} from "@/context/CartProvider"; 
+import { CombinedProvider } from "@/context/CartProvider"; 
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { SanityLive } from "@/sanity/lib/live";
-import {ClerkProvider} from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
 
 // Import the Poppins font
 const poppins = Poppins({
@@ -16,37 +16,38 @@ const poppins = Poppins({
   display: "swap", // Ensures the text uses fallback fonts until Poppins loads
 });
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: {
-    default:"Furniro",
-    template:"%s | Furniro ",
+    default: "Furniro",
+    template: "%s | Furniro ",
   },
   description: "Developed by Ahmed Noorani",
-  icons:"/logo.png"
+  icons: "/logo.png",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider >
-    <html lang="en">
-      <body className={`${poppins.className} flex flex-col min-h-screen`}>
-      <ToastContainer />
-        <CombinedProvider>
-          {/* Navbar */}
-          <Navbar />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.className} flex flex-col min-h-screen`}>
+          <ToastContainer />
+          <CombinedProvider>
+            {/* Navbar */}
+            <Navbar />
 
-          {/* Main Content */}
-          <main className="">{children}</main>
+            {/* Main Content */}
+            <main className="">{children}</main>
 
-          {/* Footer */}
-          <Footer />
-        </CombinedProvider>
-        <SanityLive/>
-      </body>
-    </html>
+            {/* Footer */}
+            <Footer />
+          </CombinedProvider>
+          <SanityLive />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
@@ -24,7 +24,9 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};  
+}
